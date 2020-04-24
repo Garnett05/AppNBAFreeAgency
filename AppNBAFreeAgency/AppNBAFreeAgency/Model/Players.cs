1618373431622789cfe2b10cdf81e9adf7a596f9
Garnett05 <charlleslimamota10@hotmail.com>
2020-04-23 21:22:18 -0300

    Class Players

---
+++ AppNBAFreeAgency/AppNBAFreeAgency/Model/Players.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppNBAFreeAgency.Model
+{
+    public class Players
+    {
+        public string Name { get; set; }
+        public short Age { get; set; }
+        public string BirthCity { get; set; }
+        public double Salary { get; set; }
+        public string Description { get; set; }
+        public bool ThreePointShooter { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
