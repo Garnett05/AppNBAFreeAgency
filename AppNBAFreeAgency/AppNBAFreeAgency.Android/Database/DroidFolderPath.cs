d16ef9cc4ba9a789cda7ca102c72fa340bf5da44
Garnett05 <charlleslimamota10@hotmail.com>
2020-04-24 21:30:41 -0300

    Android folder path for the SQLite

---
+++ AppNBAFreeAgency/AppNBAFreeAgency.Android/Database/DroidFolderPath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AppNBAFreeAgency.Database;
+//using System.Runtime.CompilerServices; // Without removing that, the assembly doesn't work
+using AppNBAFreeAgency.Droid.Database;
+using Xamarin.Forms;
+
+[assembly:Dependency(typeof(DroidFolderPath))] // need to add 'using Xamarin.Forms' to use the assembly
+namespace AppNBAFreeAgency.Droid.Database
+{
+    public class DroidFolderPath : IDatabasePath
+    {
+        public string GetPath(string DatabaseName)
+        {                        
+            //android folder path
+            string folderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string dbPath = System.IO.Path.Combine(folderPath, DatabaseName);
+            return dbPath;
+        }
+    }
+}
--- AppNBAFreeAgency/AppNBAFreeAgency/Database/IDatabasePath.cs
+++ AppNBAFreeAgency/AppNBAFreeAgency/Database/IDatabasePath.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace AppNBAFreeAgency.Database
 {
-    interface IDatabasePath
+    public interface IDatabasePath
     {
         string GetPath(string DatabaseName);
     }
